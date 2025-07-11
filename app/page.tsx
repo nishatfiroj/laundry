0c828cccd98e5b843f669a3218228c27c8e34186
Nishat Firoj <nishat.firoj@gmail.com>
2025-07-11 11:31:30 -0700

    spacing

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,9 @@ export default function Home() {
           <h2 style={{ color: "#4c5dd0", marginBottom: -12 }}>
             Your Own Professional
           </h2>
-          <h2 style={{ color: "#43c57b" }}>Local Laundromat</h2>
+          <h2 style={{ color: "#43c57b", marginBottom: 8 }}>
+            Local Laundromat
+          </h2>
         </Grid>
         <Grid size={12}>
           <ul>
